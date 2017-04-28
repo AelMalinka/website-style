5d001df4b87e5734cfd265d12b331d5e2a97e2ee
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-04-27 23:22:37 -0400

    handle /fonts/* requests, set font-path correctly

--- index.js
+++ index.js
@@ -5,13 +5,16 @@
 'use strict';
 
 const koa = require('koa');
-
 const logger = require('koa-logger');
+
 const etag = require('koa-etag');
 const compress = require('koa-compress');
 const conditional = require('koa-conditional-get');
 
 const style = require('./style.js');
+const serve = require('koa-static');
+const route = require('koa-route');
+
 const config = require('config')(require('./config.js'));
 
 const app = new koa();
@@ -22,6 +25,9 @@ app.use(logger());
 app.use(compress());
 app.use(conditional());
 app.use(etag());
+
+// 2017-04-27 AMR NOTE: router prepend and static path need to match on /fonts/
+app.use(route.get(/^\/fonts(?:\/|$)/, serve('external/bootstrap')));
 
 config.onReady(function() {
 	app.use(style({src: config.where, compress: config.compress}));
